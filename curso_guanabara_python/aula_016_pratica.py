8bb87eb28f1e4b590ede1a93e896b7b231aee8d2
Willian Lopes <lopesantic@hotmail.com>
2023-11-24 09:57:32 -0300

    .

--- curso_guanabara_python/aula_016_pratica.py
+++ curso_guanabara_python/aula_016_pratica.py
@@ -1,16 +1,24 @@
-lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita') # Tupla pode ser feita sem os parenteses no python 3
+# lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita') # Tupla pode ser feita sem os parenteses no python 3
 # !!!!!!! TUPLAS SÃO IMUTÁVEIS !!!!!!!
-
+#
 # print(lanche[::2])
 # print(len(lanche))
 #
 # for cont in range(0, len(lanche)):
 #     print(f'Eu vou comer {lanche[cont]}')
-# #
+#
 #
 # for comida in lanche:
 #     print(f'Eu vou comer {comida}')
 # print('Comi pra caramba!')
+#
+# for pos, comida in enumerate(lanche):
+#     print(f'Eu vou comer {comida} na posição {pos}')
+#
+# print(sorted(lanche))
+# print(lanche)
 
-for pos, comida in enumerate(lanche):
-    print(f'Eu vou comer {comida} na posição {pos}')
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+print(sorted(c)) # sorted coloca em ordem

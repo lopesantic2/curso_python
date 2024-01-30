0d04fb88b1873d4cbf8b02acabe5a51c0fca0c88
Willian Lopes <lopesantic@hotmail.com>
2024-01-30 12:01:01 -0300

    .retomando aulas

---
+++ curso_guanabara_python/aula_06_tiposPrimitivos.py
@@ -0,0 +1,17 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+s = n1 + n2
+
+print('A soma vale ', s)
+
+# int = numeros inteiros. ex: 7, -4, 0, 9875
+
+# float = numeros com ponto flutuante. ex: 4.5, 0.076, -15.223, 7.0
+
+# bool = booleanos. ex: True ou False (verdadeiro ou falso)
+
+# str = texto. ex: 'Olá', '7.5', ''.
+
+print('A soma vale', s)
+
+print(f'A soma vale {s}') # F-String
